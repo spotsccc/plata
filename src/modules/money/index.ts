40bd29821f3f8856d7f10febfc3d1dd651c08f44
chernenkoik <chernenkoik@tutu.tech>
2024-08-24 21:18:38 -0300

    fix build

--- src/modules/money/index.ts
+++
@@ -1,4 +0,0 @@
-export { minus } from "../finance/lib/minus";
-export { plus } from "../finance/lib/plus";
-export type { Money } from "./model";
-export { Currency, createEmptyMoney } from "./model";
